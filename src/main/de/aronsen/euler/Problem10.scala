e3325fd895a0d8179bd99e328e107768e16d9238
Andre Kvist Aronsen <andre.aronsen@gmail.com>
2010-11-04 07:15:27 +0100

    Solved problem 10

--- src/main/de/aronsen/euler/Problem10.scala
+++ src/main/de/aronsen/euler/Problem10.scala
@@ -1,44 +1,33 @@
 package de.aronsen.euler
 
-import collection.mutable.MutableList
-
 class Problem10 {
-  def calculateSumOfPrimesBelow(n: Int): Int = primesUpTo(n).sum
-
-  def primesUpTo(n: Int): MutableList[Int] = {
-    val m = Math.sqrt(n).toInt
-    println(m)
-    def sieve(xs: MutableList[Int]): MutableList[Int] = {
-      if (xs.isEmpty || xs.head > m) return xs
-
-      val p = xs.head
-      val toRemove = xs.map(n => p * n)
-      val filteredXs = xs.diff(toRemove)
-
-      return sieve(filteredXs.tail) += p
-    }
-
-
-    val list: MutableList[Int] = new MutableList
-    list ++= List.range(2, n + 1)
-
-    List.range(2, Math.sqrt(n).toInt + 1).foreach(i => {
-      if (list.contains(i)) {
-        list.foreach(j => if (j > i && j % i == 0) list.update(j - 2, 0))
-      }
-    })
-
-    //return sieve(list)
-    return list
+  def calculateSumOfPrimesBelow(n: Int): Long = {
+    2 + (3 to n by 2).foldLeft(0L)((s, i) => if (couldBePrime(i) && isPrime(i)) s + i else s)
   }
 
+  def couldBePrime(n: Int) = calculatePrimesUpTo(200).forall(p => p == n || n % p != 0)
+
+  def isPrime(n: Int) = (2 to Math.sqrt(n).toInt) forall (n % _ != 0)
+
+  def calculatePrimesUpTo(n: Int): List[Int] = {
+    def sieve(xs: List[Int]): List[Int] = {
+      if (xs.isEmpty) return Nil
+
+      val p = xs.head
+      val filteredXs = xs.diff(xs.map(n => p * n))
+
+      return p :: sieve(filteredXs.tail)
+    }
+
+    return sieve(List.range(2, n + 1))
+  }
 }
 
 object RunProblem10 {
   def main(args: Array[String]) {
     val p10 = new Problem10
     println("Solution (n=10): " + p10.calculateSumOfPrimesBelow(10))
-    println("Solution (n=1000): " + p10.calculateSumOfPrimesBelow(20000))
-    //println("Solution Problem 10 (n=2 million)" + p10.calculateSumOfPrimesBelow(2000000))
+    println("Solution (n=1000): " + p10.calculateSumOfPrimesBelow(1000))
+    println("Solution Problem 10 (n=2 million): " + p10.calculateSumOfPrimesBelow(2000000))
   }
 }
